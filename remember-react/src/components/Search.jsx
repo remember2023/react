be8eb5cc26dc99ea4d5d001a52bcde2cb68b6f25
beily <bk95212603@gmail.com>
2023-12-20 17:16:17 +0200

    basic start

--- remember-react/src/components/Search.jsx
+++ remember-react/src/components/Search.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from '@mui/material/Button';
+import { FormControl, TextField } from "@mui/material";
 
 const Search= ()=>{
+    const[objectForSearcg, setObjectForSearch]=useState('')
 
     return<>
     <h1>wellcom to search</h1>
+    <FormControl >
+         <TextField
+            dir="rtl"
+            required
+            id="outlined-required"
+            label="קיר הנצחה או שם"
+            onChange={e => setObjectForSearch(e.target.value)}
+         />
+      </FormControl> 
+      <Button></Button>
     </>
 }
 export default Search
